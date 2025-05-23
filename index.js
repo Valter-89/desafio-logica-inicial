8719e293da5bc80135e05156565c0d7b19e5087e
Valter-89 <mercurio.mktdelivery@gmail.com>
2025-05-23 15:09:07 -0300

    Atualização do projeto

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log("Tamo aí na atividade")
+let nome_heroi = "Jonas Amauri de Castro"
+let xp_heroi = 5555
+let nivel_heroi
+
+if(xp_heroi >= 1001 && xp_heroi <= 2000){
+    nivel_heroi = "Bronze"
+    console.log(nome_heroi + " tem " + xp_heroi + " de XP e está no nível " + nivel_heroi)
+}else if(xp_heroi >= 2001 && xp_heroi <= 5000){
+    nivel_heroi = "Prata"
+     console.log(nome_heroi + " tem " + xp_heroi + " de XP e está no nível " + nivel_heroi)
+}else if(xp_heroi >= 5001 && xp_heroi <= 6000){
+    nivel_heroi = "Ouro"
+     console.log(nome_heroi + " tem " + xp_heroi + " de XP e está no nível " + nivel_heroi)
+}else if(xp_heroi >= 6001 && xp_heroi <= 7000){
+    nivel_heroi = "Platina"
+     console.log(nome_heroi + " tem " + xp_heroi + " de XP e está no nível " + nivel_heroi)
+}else if(xp_heroi >= 7001 && xp_heroi <= 8000){
+    nivel_heroi = "Diamante"
+     console.log(nome_heroi + " tem " + xp_heroi + " de XP e está no nível " + nivel_heroi)
+}else if(xp_heroi >= 8001 && xp_heroi <= 9000){
+    nivel_heroi = "Ascendente"
+     console.log(nome_heroi + " tem " + xp_heroi + " de XP e está no nível " + nivel_heroi)
+}else if(xp_heroi >= 9001 && xp_heroi <= 10000){
+    nivel_heroi = "Imortal"
+     console.log(nome_heroi + " tem " + xp_heroi + " de XP e está no nível " + nivel_heroi)
+}else if(xp_heroi >= 10001){
+    nivel_heroi = "Radiante"
+     console.log(nome_heroi + " tem " + xp_heroi + " de XP e está no nível " + nivel_heroi)
+}
